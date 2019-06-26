b7dba677cfde1a7db843e291ff14f52db2e05d08
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-06-26 11:02:01 +0300

    Add new

---
+++ Advanced/Advanced/SpaceShipCrafting/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpaceshipCrafting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] liquidsElements = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToArray();
+
+            int[] itemsElements = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToArray();
+
+            Queue<int> liquids = new Queue<int>(liquidsElements);
+
+            Stack<int> items = new Stack<int>(itemsElements);
+
+            Dictionary<string, int> advancedItems = new Dictionary<string, int>();
+            advancedItems.Add("Glass", 0);
+            advancedItems.Add("Aluminium", 0);
+            advancedItems.Add("Lithium", 0);
+            advancedItems.Add("Carbon fiber", 0);
+
+            int glass = 25;
+            int aluminium = 50;
+            int lithium = 75;
+            int carbon = 100;
+
+            while (items.Count > 0 && liquids.Count > 0)
+            {
+
+                int sum = 0;
+                var currentLiquid = liquids.Peek();
+                var currentItem = items.Peek();
+
+                sum = currentItem + currentLiquid;
+
+                if (sum == glass)
+                {
+
+                    advancedItems["Glass"]++;
+
+                    liquids.Dequeue();
+                    items.Pop();
+                }
+
+
+                else if (sum == aluminium)
+                {
+
+                    advancedItems["Aluminium"]++;
+
+                    liquids.Dequeue();
+                    items.Pop();
+                }
+
+
+                else if (sum == lithium)
+                {
+
+                    advancedItems["Lithium"]++;
+                    liquids.Dequeue();
+                    items.Pop();
+                }
+
+                else if (sum == carbon)
+                {
+
+                    advancedItems["Carbon fiber"]++;
+                    liquids.Dequeue();
+                    items.Pop();
+                }
+                else
+                {
+                    liquids.Dequeue();
+                    items.Pop();
+                    items.Push(currentItem + 3);
+
+                }
+                if (items.Count == 0 || liquids.Count == 0)
+                {
+                    break;
+                }
+            }
+
+            if (advancedItems["Glass"] > 0 && advancedItems["Aluminium"] > 0 && advancedItems["Lithium"] > 0 && advancedItems["Carbon fiber"] > 0)
+            {
+                Console.WriteLine($"Wohoo! You succeeded in building the spaceship!");
+            }
+            else
+            {
+                Console.WriteLine($"Ugh, what a pity! You didn't have enough materials to build the spaceship.");
+            }
+
+            if (liquids.Any())
+            {
+                Console.WriteLine($"Liquids left: {string.Join(", ", liquids)}");
+            }
+            else
+            {
+                Console.WriteLine($"Liquids left: none");
+            }
+
+            if (items.Any())
+            {
+                Console.WriteLine($"Physical items left: {string.Join(", ", items)}");
+            }
+            else
+            {
+                Console.WriteLine($"Physical items left: none");
+            }
+
+            foreach (var item in advancedItems.OrderBy(i => i.Key))
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+
+            }
+        }
+    }
+}
---
+++ Advanced/Advanced/SpaceStationEstablished/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace SpaceStationEstablished
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[matrixSize, matrixSize];
+            int playerRow = 0;
+            int playerCol = 0;
+            int firstHoleRow = 0;
+            int firstHoleCol = 0;
+            int secondHoleRow = 0;
+            int secondHoleCol = 0;
+            int stars = 0;
+            int holeCount = 0;
+            for (int row = 0; row < matrixSize; row++)
+            {
+                char[] line = Console.ReadLine().ToCharArray();
+
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    matrix[row, col] = line[col];
+
+                    if (matrix[row, col] == 'S')
+                    {
+                        playerRow = row;
+                        playerCol = col;
+                    }
+                    if (matrix[row, col] == 'O' && holeCount == 0)
+                    {
+                        firstHoleRow = row;
+                        firstHoleCol = col;
+                        holeCount++;
+                    }
+                    else if (matrix[row, col] == 'O' && holeCount > 0)
+                    {
+                        secondHoleRow = row;
+                        secondHoleCol = col;
+                    }
+                }
+            }
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "up")
+                {
+                    matrix[playerRow, playerCol] = '-';
+                    playerRow--;
+
+                }
+
+                else if (command == "down")
+                {
+
+                    matrix[playerRow, playerCol] = '-';
+                    playerRow++;
+                }
+
+
+                else if (command == "left")
+                {
+
+                    matrix[playerRow, playerCol] = '-';
+                    playerCol--;
+
+                }
+
+                else if (command == "right")
+                {
+
+                    matrix[playerRow, playerCol] = '-';
+                    playerCol++;
+
+                }
+                if (playerRow < 0 || playerRow >= matrix.GetLength(0) || playerCol < 0 || playerCol >= matrix.GetLength(1))
+                {
+                    Console.WriteLine($"Bad news, the spaceship went to the void.");
+                    Console.WriteLine($"Star power collected: {stars}");
+                    break;
+                }
+                if (Char.IsDigit(matrix[playerRow, playerCol]))
+                {
+                    stars += int.Parse(matrix[playerRow, playerCol].ToString());
+                    matrix[playerRow, playerCol] = 'S';
+                }
+                else if (playerRow == firstHoleRow && playerCol == firstHoleCol)
+                {
+                    playerRow = secondHoleRow;
+                    playerCol = secondHoleCol;
+                    matrix[firstHoleRow, firstHoleCol] = '-';
+                    matrix[secondHoleRow, secondHoleCol] = 'S';
+                }
+                else if (playerRow == secondHoleRow && playerCol == secondHoleCol)
+                {
+                    playerRow = firstHoleRow;
+                    playerCol = firstHoleCol;
+                    matrix[secondHoleRow, secondHoleCol] = '-';
+                    matrix[firstHoleRow, firstHoleCol] = 'S';
+                }
+
+                if (stars >= 50)
+                {
+                    Console.WriteLine($"Good news! Stephen succeeded in collecting enough star power!");
+                    Console.WriteLine($"Star power collected: {stars}");
+                    break;
+                }
+
+            }
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
