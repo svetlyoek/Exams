ab22cdeacd91d8e5721f9098f5e01edd366bc644
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-08-07 18:58:11 +0300

    Add new

---
+++ OOP/DemoExam/Skeleton/MXGP/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace MXGP
+{
+    using MXGP.Core.Contracts;
+    using MXGP.Core.Models;
+    using MXGP.IO;
+    using MXGP.IO.Contracts;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            IChampionshipController controller = new ChampionshipController();
+            IWriter writer = new ConsoleWriter();
+            IReader reader = new ConsoleReader();
+
+            var engine = new Engine(controller, reader, writer);
+            engine.Run();
+
+        }
+    }
+}
