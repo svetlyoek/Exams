bbbd96b8c3a06aa0e8c4b8ee996db14c5d11a881
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-08-07 20:13:52 +0300

    Add new

---
+++ OOP/DemoExam/Skeleton/MXGP/Models/Motorcycles/Motorcycles/PowerMotorcycle.cs
@@ -0,0 +1,36 @@
+﻿namespace MXGP.Models.Motorcycles.Models
+{
+    using MXGP.Utilities.Messages;
+    using System;
+    public class PowerMotorcycle : Motorcycle
+    {
+        private const double CUBIC_CENTIMETERS = 450;
+        private const int MIN_HORSEPOWER = 70;
+        private const int MAX_HORSEPOWER = 100;
+
+        private int horsePower;
+        public PowerMotorcycle(string model, int horsePower)
+            : base(model, horsePower, CUBIC_CENTIMETERS)
+        {
+            this.HorsePower = horsePower;
+        }
+
+        public override int HorsePower
+        {
+            get
+            {
+                return this.horsePower;
+            }
+            protected set
+            {
+                if (value < MIN_HORSEPOWER || value > MAX_HORSEPOWER)
+                {
+                    throw new ArgumentException(String.Format(ExceptionMessages.InvalidHorsePower, value));
+                }
+
+                this.horsePower = value;
+            }
+        }
+
+    }
+}
