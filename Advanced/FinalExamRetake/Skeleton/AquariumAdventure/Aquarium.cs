b8a9b1fe1f207bcec409930c574a7466c5a0d140
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-08-16 08:27:51 +0300

    Add new

---
+++ Advanced/FinalExamRetake/Skeleton/AquariumAdventure/Aquarium.cs
@@ -0,0 +1,74 @@
+﻿namespace AquariumAdventure
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Aquarium
+    {
+        private List<Fish> fishInPool;
+
+        public Aquarium(string name, int capacity, int size)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.Size = size;
+            this.fishInPool = new List<Fish>();
+        }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Size { get; set; }
+
+        public void Add(Fish fish)
+        {
+            if (!this.fishInPool.Any(f => f.Name == fish.Name) && this.fishInPool.Count < this.Capacity)
+            {
+                this.fishInPool.Add(fish);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            if (this.fishInPool.Any(f => f.Name == name))
+            {
+                var fishToRemove = this.fishInPool.FirstOrDefault(f => f.Name == name);
+
+                this.fishInPool.Remove(fishToRemove);
+                return true;
+            }
+
+            return false;
+        }
+
+        public Fish FindFish(string name)
+        {
+            var fishToRemove = this.fishInPool.FirstOrDefault(f => f.Name == name);
+
+            if (this.fishInPool.Contains(fishToRemove))
+            {
+                return fishToRemove;
+            }
+
+            return null;
+
+        }
+
+        public string Report()
+        {
+
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"Aquarium: {this.Name} ^ Size: {this.Size}");
+
+            foreach (var fish in this.fishInPool)
+            {
+                result.AppendLine(fish.ToString());
+            }
+
+            return result.ToString().TrimEnd();
+        }
+
+    }
+}
---
+++ Advanced/Repository/Person.cs
@@ -0,0 +1,44 @@
+﻿
+namespace Repository
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Person
+    {
+        private string name;
+        private int age;
+        private DateTime birthdate;
+
+        public Person()
+        {
+
+        }
+
+        public Person(string name, int age, DateTime birthdate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Birthdate = birthdate;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            set => this.name = value;
+        }
+
+        public int Age
+        {
+            get => this.age;
+            set => this.age = value;
+        }
+
+        public DateTime Birthdate
+        {
+            get => this.birthdate;
+            set => this.birthdate = value;
+        }
+    }
+}
