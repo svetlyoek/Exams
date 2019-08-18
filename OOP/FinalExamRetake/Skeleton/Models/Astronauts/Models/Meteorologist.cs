7ef7203760e570e45701879838da40c2ebc373e5
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-08-18 19:32:28 +0300

    Add new

---
+++ OOP/FinalExamRetake/Skeleton/Models/Astronauts/Models/Meteorologist.cs
@@ -0,0 +1,12 @@
+﻿namespace SpaceStation.Models.Astronauts.Models
+{
+    public class Meteorologist : Astronaut
+    {
+        private const double InitialOxygen = 90;
+
+        public Meteorologist(string name)
+            : base(name, InitialOxygen)
+        {
+        }
+    }
+}
