877dcdea28ae9c10e2b405c558468d2082e17413
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-12-03 12:13:01 +0200

    Remove item

--- DB/Entity framework core/Exam prep I/Skeleton/Cinema/Cinema/Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Cinema.Data
-{
-   public static class Configuration
-    {
-        public static string ConnectionString = @"Server=DESKTOP-JH4M4M9\MSSQLSERVER01;Database=Cinema;Trusted_Connection=True";
-    }
-}
