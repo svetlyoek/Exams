3ef62e90c6f0ae78466ad2141e40b951dd8cf23e
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-12-03 12:13:23 +0200

    Add new

---
+++ DB/Entity framework core/Exam prep I/Skeleton/Cinema/Cinema/Data/Models/Seat.cs
@@ -0,0 +1,16 @@
+﻿namespace Cinema.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Seat
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int HallId { get; set; }
+
+        public Hall Hall { get; set; }
+
+    }
+}
