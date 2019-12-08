acd3ac3b6b2933428116d057f053ad61d3cf2c7d
svetlyoek <svetlyo_svetlyo@abv.bg>
2019-12-08 10:38:03 +0200

    Add new

---
+++ DB/Entity framework core/Final exam/Skeleton/TeisterMask/DataProcessor/ExportDto/ExportProjectDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace TeisterMask.DataProcessor.ExportDto
+{
+    [XmlType("Project")]
+    public class ExportProjectDto
+    {
+        [XmlAttribute(AttributeName = "TasksCount")]
+        public int TaskCount { get; set; }
+
+        [XmlElement(ElementName = "ProjectName")]
+        public string ProjectName { get; set; }
+
+        [XmlElement(ElementName = "HasEndDate")]
+        public string HasEndDate { get; set; }
+
+        [XmlArray(ElementName = "Tasks")]
+        public ExportTaskDto[] Tasks { get; set; }
+    }
+}
