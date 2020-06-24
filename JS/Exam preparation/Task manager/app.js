d1256247160b2404731d1d3be3b24f266450dda9
svetlyoek <svetlyo_svetlyo@abv.bg>
2020-06-24 16:30:17 +0300

    Add new

--- JS/Exam preparation/Task manager/app.js
+++ JS/Exam preparation/Task manager/app.js
@@ -2,10 +2,9 @@ function solve() {
 
     const taskField = document.getElementById('task');
     const descriptionField = document.getElementById('description');
-    const dateFiled = document.getElementById('date');
+    const dateField = document.getElementById('date');
     const addBtn = document.getElementById('add');
 
-    const addDiv = document.querySelector('.wrapper section:nth-child(1) div:nth-child(2)');
     const openDiv = document.querySelector('.wrapper section:nth-child(2) div:nth-child(2)');
     const progressDiv = document.querySelector('.wrapper section:nth-child(3) div:nth-child(2)');
     const completeDiv = document.querySelector('.wrapper section:nth-child(4) div:nth-child(2)');
@@ -13,7 +12,7 @@ function solve() {
     addBtn.addEventListener('click', function (e) {
         e.preventDefault();
 
-        if (taskField.value !== '' && descriptionField.value !== '' && dateFiled.value !== '') {
+        if (taskField.value !== '' && descriptionField.value !== '' && dateField.value !== '') {
 
             const article = document.createElement('article');
             const heading3 = document.createElement('h3');
@@ -22,12 +21,14 @@ function solve() {
             const divFlex = document.createElement('div');
             const startBtn = document.createElement('button');
             const deleteBtn = document.createElement('button');
+            const finishBtn = document.createElement('button');
 
-            heading3.textContent = taskField.value;
-            descriptionParagraph.textContent = descriptionField.value;
-            dateParagraph.textContent = dateFiled.value;
+            heading3.textContent = taskField.value.trim();
+            descriptionParagraph.textContent = `Description: ` + descriptionField.value.trim();
+            dateParagraph.textContent = `Due Date: ` + dateField.value.trim();
             startBtn.textContent = 'Start';
             deleteBtn.textContent = 'Delete';
+            finishBtn.textContent = 'Finish';
 
             article.appendChild(heading3);
             article.appendChild(descriptionParagraph);
@@ -36,6 +37,7 @@ function solve() {
             divFlex.classList.add('flex');
             startBtn.classList.add('green');
             deleteBtn.classList.add('red');
+            finishBtn.classList.add('orange');
 
             divFlex.appendChild(startBtn);
             divFlex.appendChild(deleteBtn);
@@ -47,48 +49,25 @@ function solve() {
             document.getElementById('task').value = '';
             document.getElementById('description').value = '';
             document.getElementById('date').value = '';
+
+            startBtn.addEventListener('click', function (e) {
+
+                progressDiv.appendChild(article);
+                startBtn.remove();
+                divFlex.appendChild(finishBtn);
+            });
+
+            finishBtn.addEventListener('click', function (e) {
+
+                completeDiv.appendChild(article);
+
+                divFlex.remove();
+            });
+
+            deleteBtn.addEventListener('click', function (e) {
+
+                article.remove();
+            });
         }
-    });
-
-    openDiv.addEventListener('click', function (e) {
-
-        const pressedBtn = e.target;
-        const currentArticle = pressedBtn.closest('article');
-
-        if (pressedBtn.textContent === 'Start') {
-
-            const currStartBtn = currentArticle.querySelector('div.flex button.green');
-            const currDeleteBtn = currentArticle.querySelector('div.flex button.red');
-            currStartBtn.classList.remove('green');
-            currStartBtn.classList.add('red');
-            currDeleteBtn.classList.remove('red');
-            currDeleteBtn.classList.add('orange');
-            currStartBtn.textContent = 'Delete';
-            currDeleteBtn.textContent = 'Finish';
-            progressDiv.appendChild(currentArticle);
-
-        } else if (pressedBtn.textContent === 'Delete') {
-
-            currentArticle.remove();
-        }
-    });
-
-    progressDiv.addEventListener('click', function (e) {
-
-        const pressedBtn = e.target;
-        const currentArticle = pressedBtn.closest('article');
-
-        if (pressedBtn.textContent === 'Delete') {
-
-            currentArticle.remove();
-        } else if (pressedBtn.textContent === 'Finish') {
-
-            const currentDiv = pressedBtn.closest('div');
-
-            currentDiv.remove();
-
-            completeDiv.appendChild(currentArticle);
-        }
-
     });
 }
